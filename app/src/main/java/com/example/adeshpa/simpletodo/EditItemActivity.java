42c7ed8efe0cfb962bde875337a3e0738ea4b986
Anjali Deshpande <adeshpande@walmartlabs.com>
2016-05-02 23:33:33 -0700

    Code to Add/Delete/Edit items from todo list

---
+++ app/src/main/java/com/example/adeshpa/simpletodo/EditItemActivity.java
@@ -0,0 +1,40 @@
+package com.example.adeshpa.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_edit_item);
+
+        String name = this.getIntent().getStringExtra("itemText");
+        position = this.getIntent().getIntExtra("itemPosition", -1);
+
+        EditText saveItem = (EditText) findViewById(R.id.etSaveItem);
+        saveItem.setText(name);
+        saveItem.setSelection(saveItem.getText().length());
+    }
+
+    public void saveItem(View v) {
+        EditText etSaveItem = (EditText) findViewById(R.id.etSaveItem);
+        String itemText = etSaveItem.getText().toString();
+
+        Intent appInfo = new Intent(EditItemActivity.this, MainActivity.class);
+        appInfo.putExtra("itemText", itemText);
+        appInfo.putExtra("itemPosition", position);
+        startActivity(appInfo);
+    }
+
+}
---
+++ app/src/main/java/com/example/adeshpa/simpletodo/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.adeshpa.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lstItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String itemText = this.getIntent().getStringExtra("itemText");
+        int itemPosition = this.getIntent().getIntExtra("itemPosition", -1);
+
+        lstItems = (ListView) findViewById(R.id.lstItems);
+        items = new ArrayList<>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lstItems.setAdapter(itemsAdapter);
+
+        //items.add("First Item");
+        //items.add("Second Item");
+        if (itemPosition >= 0) {
+            items.set(itemPosition, itemText);
+            writeItems();
+        }
+        setupListViewListener();
+        setupItemListener();
+    }
+
+    private void setupListViewListener() {
+        lstItems.setOnItemLongClickListener( new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id)  {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+
+   private void setupItemListener() {
+        lstItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+                String text = items.get(position);
+                Intent appInfo = new Intent(MainActivity.this, EditItemActivity.class);
+                appInfo.putExtra("itemText", text);
+                appInfo.putExtra("itemPosition", position);
+                startActivity(appInfo);
+            }
+        });
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
